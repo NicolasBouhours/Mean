a6e7ad3a7fb0d225a129566db3733b6ee4d9e401
NicolasBouhours <nico53960@hotmail.fr>
2016-12-04 15:19:08 +0100

    Setting Up Fontawesome and Basic Header

--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    template: `
+    /*template: `
         <header class="row">
             <nav class="col-md-8 col-md-offset-2">
                 <ul class="nav nav-pills">
@@ -11,7 +11,8 @@ import { Component } from '@angular/core';
                 </ul>
             </nav>
         </header>  
-    `
+    `*/
+    templateUrl: '/header.component.html'
 })
 export class HeaderComponent {
 
